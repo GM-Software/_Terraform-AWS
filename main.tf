a3b3651ef794e28eff9743b810845cc0f8702a5a
BXGrzesiek <grzegorz.biskup2015@gmail.com>
2021-02-20 19:23:04 +0100

    main.tf updates

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 provider "aws" {
     version = "2.31.0"
 }
+
+resource "aws_route53_zone" "kd-zsf" {
+    name = "kd-zsf.szkoladevops.com"
+}
+
+output "kd_zsf_ns" {
+    value = "${aws_route53_zone.kd_zsf_ns.name_servers}"
+}
